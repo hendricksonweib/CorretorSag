952d3c13ea6eb64fa4693373e949359745c9f6fc
hendricksonweib <hendricksonrocha22@gmail.com>
2025-08-13 14:08:33 -0300

    config: vite config fix

--- corretor/vite.config.ts
+++ corretor/vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  build: {
+    // Ajusta o limite de tamanho dos chunks para evitar o aviso
+    chunkSizeWarningLimit: 1000, // Aumente para 1000 KB ou mais se necessário
+
+    // Opções do Rollup para controle manual de chunks
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';  // Dependências de node_modules irão para 'vendor.js'
+          }
+        }
+      }
+    }
+  }
 });
